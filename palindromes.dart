0561545262acdd21d73a466abf82835758d42625
Maikhaa <maikha.abarca@gmail.com>
2019-09-24 17:07:01 +0800

    palindromes checking for null and empty values

--- palindromes.dart
+++ palindromes.dart
@@ -1,11 +1,17 @@
 bool isPalindrome(String string) {
-  var reversedHalfString = '';
-  var charList = string.split('');
-  for (var i = 0; i < charList.length ~/ 2; i++) {
-    reversedHalfString = charList[i] + reversedHalfString;
+  if (isNullOrEmpty(string)) {
+    throw 'Invalid input';
+  } else {
+    var reversedHalfString = '';
+    var charList = string.split('');
+    for (var i = 0; i < charList.length ~/ 2; i++) {
+      reversedHalfString = charList[i] + reversedHalfString;
+    }
+    return string.endsWith(reversedHalfString);
   }
-  return string.endsWith(reversedHalfString);
 }
+
+bool isNullOrEmpty(String string) => string == null || string.isEmpty;
 
 main() {
   print(isPalindrome('abvba'));
