321605a0e11f7ab795b6625da9ea5ec30c08331d
Maikhaa <maikha.abarca@gmail.com>
2019-10-02 19:10:49 +0800

    chunk added

---
+++ chunk.dart
@@ -0,0 +1,28 @@
+List<List> chunk(List<int> list, int size) {
+  List<List> chunked = [];
+  var chunk = [];
+  var rest = [];
+
+  for (int item in list) {
+    if (chunk.length < size) {
+      chunk.add(item);
+    }
+    if (chunk.length == size) {
+      chunked.add(chunk);
+      chunk = [];
+    }
+    if (list.length - list.indexOf(item) < size) {
+      rest = chunk;
+    }
+  }
+
+  if (rest.isNotEmpty) {
+    chunked.add(rest);
+  }
+
+  return chunked;
+}
+
+main() {
+  print(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
+}
