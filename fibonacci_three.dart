a86c816f2458415180d1b200967042cc5b599f8a
Maikhaa <maikha.abarca@gmail.com>
2019-11-20 17:27:52 +0800

    fibonacci_three mistery added

---
+++ fibonacci_three.dart
@@ -0,0 +1,23 @@
+int fib(int number, [int index = 2, int value = 0, List list]) {
+  if (list == null) {
+    list = [0, 1];
+  }
+
+  if (number == 0 || number == 1) {
+    return list[number];
+  }
+
+  if (index > number) {
+    print(list[number]);
+    return list[number];
+  }
+
+  var value = list[index - 1] + list[index - 2];
+  list.add(value);
+
+  fib(number, index + 1, value, list);
+}
+
+void main() {
+  print(fib(6));
+}
