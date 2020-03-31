a6942259d9f28ba290e5c21bf6f035da276309da
Maikhaa <maikha.abarca@gmail.com>
2020-03-30 21:53:42 -0500

    validate_sudoku.dart

---
+++ validate_sudoku.dart
@@ -0,0 +1,42 @@
+bool validSudoku(List<List> board) {
+  List<Set> rowSets = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+  List<Set> columnSets = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+  List<Set> boxSets = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+
+  for (List row in board) {
+    for (String string in row) {
+      if (string != ".") {
+        var number = int.parse(string);
+        if (number < 1 || number > 9) return false;
+
+        var rowIndex = board.indexOf(row);
+        var columnIndex = row.indexOf(string);
+        var boxIndex = (columnIndex ~/ 3) + 3 * (rowIndex ~/ 3);
+
+        if (checkIfUnique(rowSets, rowIndex, number) == false ||
+            checkIfUnique(columnSets, columnIndex, number) == false ||
+            checkIfUnique(boxSets, boxIndex, number) == false) return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+bool checkIfUnique(List<Set> setsList, int index, int number) {
+  return (setsList[index].contains(number) == true) ? false : setsList[index].add(number);
+}
+
+main() {
+  print(validSudoku([
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+  ]));
+}
