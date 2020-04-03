cf81cc1aff04cd182785aff03493826082496aee
Maikhaa <maikha.abarca@gmail.com>
2020-04-03 11:48:15 -0500

    happy_number added

---
+++ happy_number.dart
@@ -0,0 +1,19 @@
+bool happyNumber(int number) {
+  if (number == null) return null;
+
+  var stringList = number.toString().split('');
+  var numberList = stringList.map(int.parse).toList();
+
+  var result = 0;
+
+  for (var i = 0; i < numberList.length; i++) {
+    numberList[i] = numberList[i] * numberList[i];
+    result = result + numberList[i];
+  }
+
+  return result == 1 ? true : happyNumber(result);
+}
+
+main() {
+  print(happyNumber(19));
+}
