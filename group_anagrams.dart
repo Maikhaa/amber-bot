ae002c607bbe9a454f31be1d6dd0fce51171e668
Maikhaa <maikha.abarca@gmail.com>
2020-04-07 11:19:37 -0500

    group_anagrams

---
+++ group_anagrams.dart
@@ -0,0 +1,21 @@
+List groupAnagrams(List list) {
+  var map = {};
+
+  for (String word in list) {
+    var charList = word.split('');
+    charList.sort();
+    var key = charList.join();
+
+    if (map.containsKey(key)) {
+      map[key].add(word);
+    } else {
+      map[key] = [word];
+    }
+  }
+
+  return map.values.toList();
+}
+
+void main() {
+  print(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+}
