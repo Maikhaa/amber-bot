8f8ef61f54ec4f0a382f00d93e424b606d4f35d3
Maikhaa <maikha.abarca@gmail.com>
2020-05-20 06:59:50 -0500

    jump game

---
+++ jump_game.dart
@@ -0,0 +1,16 @@
+bool jumpGame(List list) {
+  if (list.isEmpty || list == null) return false;
+
+  var getToIndex = list.length - 1;
+
+  for (int i = list.length - 1; i >= 0; i--) {
+    if (i + list[i] >= getToIndex) getToIndex = i;
+  }
+
+  return getToIndex == 0;
+}
+
+void main() {
+  print(jumpGame([3, 2, 4, 1, 0]));
+  print(jumpGame([3, 2, 1, 0, 4]));
+}
