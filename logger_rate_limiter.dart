9c94e9dbf0f49fdce1258f92e67ca388bbd1787b
Maikhaa <maikha.abarca@gmail.com>
2020-05-22 16:24:06 -0500

    logger_rate_limiter

---
+++ logger_rate_limiter.dart
@@ -0,0 +1,29 @@
+class Logger {
+  Map map = {};
+
+  bool shouldPrintMessage(String message, int timestamp) {
+    if (message.isEmpty || message == null || timestamp == null) return false;
+
+    if (map.containsKey(message)) {
+      if (timestamp - map[message] < 10) {
+        return false;
+      } else {
+        map[message] = timestamp;
+      }
+    } else {
+      map[message] = timestamp;
+    }
+
+    return true;
+  }
+}
+
+void main() {
+  var logger = Logger();
+  print(logger.shouldPrintMessage("foo", 1));
+  print(logger.shouldPrintMessage("bar", 2));
+  print(logger.shouldPrintMessage("foo", 3));
+  print(logger.shouldPrintMessage("bar", 8));
+  print(logger.shouldPrintMessage("foo", 10));
+  print(logger.shouldPrintMessage("foo", 11));
+}
