b867134d8ad44b12bf7a42d4d294e15055daecd9
Maikhaa <maikha.abarca@gmail.com>
2020-08-11 09:04:32 -0500

    word cloud

---
+++ word_cloud.dart
@@ -0,0 +1,41 @@
+// Create a function that takes a string and returns a Map with the words in the
+// string as keys and the number of occurrences of each word as values.
+
+Map wordCloud(String string) {
+  if (string == null) return {};
+
+  Map map = {};
+
+  var startWordIndex = 0;
+  var wordLength = 0;
+
+  for (int i = 0; i < string.length; i++) {
+    var char = string[i].toLowerCase();
+
+    if (isLetter(char)) {
+      if (wordLength == 0) {
+        startWordIndex = i;
+      }
+      wordLength++;
+    }
+
+    if ((!isLetter(char) || i == string.length - 1) && wordLength > 0) {
+      var word = string.substring(startWordIndex, startWordIndex + wordLength);
+      if (map.containsKey(word)) {
+        map[word]++;
+      } else {
+        map[word] = 1;
+      }
+      wordLength = 0;
+    }
+  }
+  return map;
+}
+
+bool isLetter(String char) =>
+    (char.codeUnitAt(0) >= 97 && char.codeUnitAt(0) <= 122) || char == '\'';
+
+main() {
+  print(wordCloud(
+      "Apple pie, lemon dessert, chocolate dessert, vanilla cookies, chocolate cake.")); // true
+}
